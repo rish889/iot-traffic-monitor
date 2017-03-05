69144d6d38d64e87b9f40005441790b787544707
Garcha <rish889@gmail.com>
2017-03-05 00:21:17 -0800

    DockerCompose

--- iot-kafka-producer/src/main/resources/entryPoint.sh
+++ iot-kafka-producer/src/main/resources/entryPoint.sh
@@ -2,6 +2,6 @@
 
 /zookeeper-3.4.9/bin/zkServer.sh start-foreground &
 sleep 2
-/kafka_2.11-0.8.2.1/bin/kafka-server-start.sh /kafka_2.11-0.8.2.1/config/server.properties &
-sleep 2
-/bin/bash
+/kafka_2.11-0.8.2.1/bin/kafka-server-start.sh /kafka_2.11-0.8.2.1/config/server.properties
+#sleep 5
+#/bin/bash
