9ca93b19fa7968f5d39574bfda25bb4022680a05
Garcha <rish889@gmail.com>
2017-03-05 18:17:11 -0800

    iot-kafka-producer

--- iot-kafka-producer/README.md
+++ iot-kafka-producer/README.md
@@ -4,10 +4,6 @@ docker build -t rish889/iot-kafka-producer .
 
 docker rm iot-kafka-producer
 
-docker run --name iot-traffic-monitor -it rish889/iot-kafka-producer
+docker run --name iot-kafka-producer -it rish889/iot-kafka-producer
 
 docker exec -it iot-kafka-producer bash
-
-echo "Hello, World" | /kafka_2.11-0.8.2.1/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic TutorialTopic > /dev/null
-
-/kafka_2.11-0.8.2.1/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic TutorialTopic --from-beginning
--- iot-kafka-producer/src/main/resources/entryPoint.sh
+++ iot-kafka-producer/src/main/resources/entryPoint.sh
@@ -2,6 +2,15 @@
 
 /zookeeper-3.4.9/bin/zkServer.sh start-foreground &
 sleep 2
-/kafka_2.11-0.8.2.1/bin/kafka-server-start.sh /kafka_2.11-0.8.2.1/config/server.properties
-#sleep 5
-#/bin/bash
+/kafka_2.11-0.8.2.1/bin/kafka-server-start.sh /kafka_2.11-0.8.2.1/config/server.properties &
+sleep 5
+
+#echo "Hello, World" | /kafka_2.11-0.8.2.1/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic TutorialTopic
+#/kafka_2.11-0.8.2.1/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic iot-data-event --from-beginning
+
+/kafka_2.11-0.8.2.1/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic iot-data-event
+sleep 2
+
+java -jar  /target/iot-kafka-producer-1.0-SNAPSHOT.jar
+
+# /bin/bash
